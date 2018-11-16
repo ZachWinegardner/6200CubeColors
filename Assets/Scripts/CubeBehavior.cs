b855386c1fb4d73c1c190031eefe024400ee847e
Zach Winegardner <winegardner.55@osu.edu>
2018-11-16 11:32:06 -0500

    initial commit

---
+++ Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour {
+
+
+    Material cubeMat;
+    public Color goodCubeColor, badCubeColor, defaultCubeColor; 
+
+
+    //This function says: "if you enter another object's trigger, look at that thing's collider component, and store it in the "thingTriggered" variable
+    private void OnTriggerEnter(Collider thingTriggered)
+    {
+        cubeMat = thingTriggered.gameObject.GetComponent<Renderer>().material;
+     
+        if (thingTriggered.gameObject.CompareTag("good"))
+        {
+            cubeMat.color = goodCubeColor;
+
+        }
+        else
+        {
+            cubeMat.color = badCubeColor; 
+        }
+    }
+
+    //This function runs when you leave a trigger, and it stores that thing in "thingExited"
+    private void OnTriggerExit(Collider thingExited)
+    {
+        thingExited.GetComponent<Renderer>().material.color = defaultCubeColor; 
+    }
+
+    
+}
